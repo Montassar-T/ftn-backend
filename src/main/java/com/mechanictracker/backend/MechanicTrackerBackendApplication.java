ba50db2a6c2dc02dbca7fa5b447bce8120aec0a4
Montassar Tayari <montassartayari14@gmail.com>
2026-01-31 23:57:33 +0100

    Init backend project

---
+++ src/main/java/com/mechanictracker/backend/MechanicTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mechanictracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mechanictracker/backend/MechanicTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechanictracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
